cd5eaa994fcdb9e3819084ba99783aff62de69ae
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-09-19 21:00:12 -0400

    Create blink.ino

---
+++ blink.ino
@@ -0,0 +1,17 @@
+// Created by: Jaden Plugowsky
+// Created on: Sept 2023
+//
+// This is a basic blink function
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
